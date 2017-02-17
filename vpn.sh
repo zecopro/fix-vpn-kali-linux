ec42cf3bc1dc3c3f5c680ba81604e04761e5baf4
zecopro <zecoa555@gmail.com>
2017-02-17 20:29:59 +0300

    Update vpn.sh

--- vpn.sh
+++ vpn.sh
@@ -7,7 +7,7 @@ then
 
 echo "# For source package access, uncomment the following line
 deb http://http.kali.org/kali kali-rolling main contrib non-free
-# deb-src http://http.kali.org/kali kali-rolling main contrib non-free" > /etc/$
+# deb-src http://http.kali.org/kali kali-rolling main contrib non-free" > /etc/apt/sources.list
 fi
 sleep 1
 apt-get update
